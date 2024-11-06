5621b4363dab25f005f7b968da173cb2eb9ab6c9
Halil Rodoplu <halil.rodoplu@mepsan.com.tr>
2024-11-06 16:40:25 +0300

    appwrite settings were created. session, middleware & queries has been created.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,28 @@
+"use client"
+
+import {useCurrent} from "@/features/auth/api/use-current"
+import {useEffect} from "react";
+import {useRouter} from "next/navigation";
+import {useLogout} from "@/features/auth/api/use-logout";
 import {Button} from "@/components/ui/button";
-import {Input} from "@/components/ui/input";
 
 export default function Home() {
+
+    const router = useRouter()
+    const {data, isLoading} = useCurrent()
+    const {mutate} = useLogout()
+
+    useEffect( () => {
+        if (!data && !isLoading){
+            router.push("/sign-in")
+        }
+    }, [data])
     return (
-        <div className="flex flex-col m-2 gap-2">
-            <Input placeholder="write smth"/>
-            <Button variant="primary" size="xs">Click</Button>
-            <Button variant="secondary">Click</Button>
-            <Button variant="destructive">Click</Button>
-            <Button variant="ghost">Click</Button>
-            <Button variant="muted">Click</Button>
-            <Button variant="outline">Click</Button>
-            <Button variant="teritary">Click</Button>
+        <div>
+            Visible to authorized users
+            <Button onClick={() => mutate()}>
+                Logout
+            </Button>
         </div>
     );
 }
